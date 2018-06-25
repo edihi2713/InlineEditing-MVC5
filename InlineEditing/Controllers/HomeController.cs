da33f68897d38653a3da713dbfe244f4188071c2
Edison Higuita <edhiguitah@grupokonecta.com>
2018-06-25 09:30:09 -0500

    Commit inicial

---
+++ InlineEditing/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using InlineEditing.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InlineEditing.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            Meucci3Entities entities = new Meucci3Entities();
+
+            return View(entities.Dominios);
+        }
+
+        [HttpPost]
+        public JsonResult insertDominio(Dominios dominio)
+        {
+
+            dominio.domFechaCreacion = DateTime.Now;
+            dominio.domFechaDesde = DateTime.Now;
+
+            //using (Meucci3Entities entities = new Meucci3Entities())
+            //{
+
+            //    entities.Dominios.Add(dominio);
+            //    entities.SaveChanges();
+            //}
+
+            return Json(dominio);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateDominio(Dominios dominio)
+        {
+            using (Meucci3Entities entities = new Meucci3Entities())
+            {
+                Dominios entity = entities.Dominios.Find(dominio.domDominioId);
+
+                entity.domDescripcion = dominio.domDescripcion;
+
+                //entities.SaveChanges();
+            }
+
+            return new EmptyResult();
+
+        }
+
+
+        [HttpPost]
+        public ActionResult DeleteDominio(int dominioId)
+        {
+            using (Meucci3Entities entities = new Meucci3Entities())
+            {
+                Dominios entity = entities.Dominios.Find(dominioId);
+                //entities.Dominios.Remove(entity);
+                //entities.SaveChanges();
+            }
+            return new EmptyResult();
+        }
+
+
+
+	}
+}
---
+++ InlineEditing/Models/Dominio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InlineEditing.Models
+{
+    public class Dominio
+    {
+        public int domDominioId { get; set; }
+        public string domDescripcion { get; set; }
+        public string domObservacion { get; set; }
+        public bool domAnulado { get; set; }
+        public Nullable<System.DateTime> domFechaCreacion { get; set; }
+        public Nullable<int> empEmpleadoIdCreador { get; set; }
+        public System.DateTime domFechaDesde { get; set; }
+        public Nullable<System.DateTime> domFechaHasta { get; set; }
+    }
+}
